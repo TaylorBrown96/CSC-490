a9206730d79add518355be9ba0f63f5a40b63d1a
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2025-02-26 03:37:08 -0500

    Update assembleDataset.py

--- _Archive/assembleDataset.py
+++ _Archive/assembleDataset.py
@@ -26,31 +26,7 @@ def main():
                     shutil.move(base + '/' + f + '/transcripts/' + transcript, base + '/transcripts')
                 shutil.rmtree(base + '/' + f + '/transcripts')
             shutil.rmtree(base + '/' + f)
-    
-    
-    """# Unzip all the folders in the dataset
-    for f in os.listdir('./_Archive'):
-        if f.endswith('.zip'):
-            with zipfile.ZipFile(f, 'r') as z:
-                z.extractall(f[:-4])
-                
-    # Go into each folder and move the contents of the audio and 
-    # transcript folders to the main folder
-    os.mkdir('audio')
-    os.mkdir('transcripts')
-    
-    for f in os.listdir('./_Archive'):
-        if os.path.isdir(f) and f != 'audio' and f != 'transcripts' and f != f.endswith('.zip'):
-            if os.path.exists(f + '/audio'):
-                for audio in os.listdir(f + '/audio'):
-                    shutil.move(f + '/audio/' + audio, 'audio')
-                shutil.rmtree(f + '/audio')
-            if os.path.exists(f + '/transcripts'):
-                for transcript in os.listdir(f + '/transcripts'):
-                    shutil.move(f + '/transcripts/' + transcript, 'transcripts')
-                shutil.rmtree(f + '/transcripts')
-            shutil.rmtree(f)"""
-            
+
 
 if __name__ == '__main__':
     main()
